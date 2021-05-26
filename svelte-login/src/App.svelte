1083c40f27c7b76fae0661caa89d0dbdaaffd211
Claudio <45590671+claudiomanno@users.noreply.github.com>
2021-05-27 01:01:16 +0200

    Add files via upload

---
+++ svelte-login/src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+	import {Router} from '@sveltech/routify';
+	import {routes} from '@sveltech/routify/tmp/routes';
+
+</script>
+<svelte:head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+</svelte:head>
+<Router {routes}/>
---
+++ svelte-login/src/enviroments/enviroments.ts
@@ -0,0 +1,11 @@
+const environment = {
+    production: false,
+    urlRegistra:'',
+    urlLoginp:'/api/backend/login',
+    urlLoginl:'https://cmannocg-qahd7fgfeti-apigcp.nimbella.io/api/backend/login',
+    urlProdotti:'',
+    urlAboutImg: '',
+    urlLogoutImg: ''
+
+  };
+  export default environment;
